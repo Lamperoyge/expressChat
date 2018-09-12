4ab30d0a2b64fa8ee115589a5d7208ef045586f6
Adrian MUSTEA <adrian.mustea@zitec.com>
2018-09-12 14:41:45 +0300

    validation for msgs

--- client/Components/Chat.js
+++ client/Components/Chat.js
@@ -7,12 +7,14 @@ export default class Chat extends React.Component {
         this.state = {
             username: '',
             message: '',
-            messages: [],
+            id: 0,
+            errMsg: '',
+            messages: []
         };
-
+        
         this.socket = io();
         this.socket.on('receiveMsg', (data) => {
-            // this.messages.push(data)
+            console.log(data)
             let joined = this.state.messages.concat(data)
             this.setState({
                 messages: joined
@@ -23,20 +25,26 @@ export default class Chat extends React.Component {
 
     sendMessage = (ev) => {
         ev.preventDefault;
-        this.socket.emit('sendMessage', {
-            username: this.state.username,
-            message: this.state.message
-        })
-        this.setState({
-            message: ''
-        })
+        if(this.state.username !== '' && this.state.message !== '') {
+            this.setState({
+                id: this.id + 1
+            })
+            this.socket.emit('sendMessage', {
+                username: this.state.username,
+                message: this.state.message,
+                id: this.id
+            })
+            this.setState({
+                message: '',
+                errMsg: ''
+            })
+        }
+        else {
+            this.setState({
+                errMsg: "enter your name and msg mofo"
+            })
+        }
     }
-
-    // connect = () => {
-    //     this.socket.emit('user', function(data) {
-    //         data = this.username
-    //     })
-    // }
 
     render() {
         
@@ -45,12 +53,15 @@ export default class Chat extends React.Component {
                 <input type="text" placeholder="Enter your username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})}></input>
                 <input type="text" placeholder="Type your message" value={this.state.message} onChange = {ev => this.setState({message: ev.target.value})}></input>
                 <input type="Submit" onClick={this.sendMessage}></input>
+                <span>{this.state.errMsg}</span>
                 <div className = "messages">
                     {this.state.messages.map(
+                        
                         message => {
+                            console.log(message.id)
                             return(
                                 <ul>
-                                    <li>{message.username} {message.message}</li>
+                                    <li>{message.username} {message.message} {message.id}</li>
                                 </ul>
                             )
                         }
